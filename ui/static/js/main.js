1b5959375e4c742b035eca0528376be875b1bd3a
German Viescas <germanviescas@gmail.com>
2023-07-11 17:56:50 -0300

    Handle HTMX errors

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,8 +1,18 @@
-var navLinks = document.querySelectorAll("nav a");
+var navLinks = document.querySelectorAll("nav a")
 for (var i = 0; i < navLinks.length; i++) {
-	var link = navLinks[i]
-	if (link.getAttribute('href') == window.location.pathname) {
-		link.classList.add("live");
-		break;
-	}
+  var link = navLinks[i]
+  if (link.getAttribute("href") == window.location.pathname) {
+    link.classList.add("live")
+    break
+  }
+}
+
+htmx.on("htmx:responseError", handleHtmxError)
+htmx.on("htmx:sendError", handleHtmxError)
+
+function handleHtmxError(ev) {
+  const el = document.getElementById("error-notification")
+  el.innerText = ev.detail.xhr.responseText
+  el.classList.remove("hidden")
+  setTimeout(() => el.classList.add("hidden"), 5_000)
 }
