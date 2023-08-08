336f92e4bb572831989e5a4a43b9ae35b5a42e29
German Viescas <germanviescas@gmail.com>
2023-08-08 12:20:42 -0300

    Add information to landing page

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"domainator/internal/config"
 	"domainator/internal/httphelp"
 	"domainator/internal/logger"
 	"domainator/internal/tmpl"
@@ -40,7 +41,13 @@ func buildServer(addr string) (*http.Server, *httprouter.Router) {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	certInterval := config.GetDuration("CHECK_CERT_INTERVAL")
+	expiryThreshold := config.GetDuration("CERT_EXPIRY_THRESHOLD")
+	pingInterval := config.GetDuration("PING_TICK_INTERVAL")
 	templateData := tmpl.BaseData(r)
+	templateData["CertCheckInterval"] = certInterval.Hours()
+	templateData["ExpirationThreshold"] = expiryThreshold.Hours() / 24
+	templateData["PingInterval"] = pingInterval.Minutes()
 	tmpl.RenderPage(w, http.StatusOK, "home.html.tmpl", &templateData)
 }
 
