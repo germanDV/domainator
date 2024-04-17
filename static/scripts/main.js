abb73b416290e91dc81d127c3be76df039db75b7
German Viescas <germanviescas@gmail.com>
2024-04-17 11:44:32 -0300

    notify about 429 errors

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,18 +1,20 @@
-document.addEventListener('htmx:beforeRequest', (ev) => {
+document.addEventListener("htmx:beforeRequest", (ev) => {
   // Before a request is made, clear out any error messages.
-  const el = ev.srcElement.parentElement.querySelector('#error')
+  const el = ev.srcElement.parentElement.querySelector("#error")
   if (el) {
-    el.innerHTML = ''
+    el.innerHTML = ""
   }
 })
 
-document.addEventListener('htmx:afterRequest', (ev) => {
+document.addEventListener("htmx:afterRequest", (ev) => {
   const code = ev.detail.xhr.status
   if (code < 400) {
     // On a successful response, reset the form (if it was a form that triggered the request).
     ev.target?.reset && ev.target.reset()
   } else if (code === 401) {
     notify("Unauthorized")
+  } else if (code === 429) {
+    notify("Service under heavy load, come back later.")
   } else if (code >= 500) {
     notify("Something went wrong. Please try again later.")
   }
@@ -21,8 +23,8 @@ document.addEventListener('htmx:afterRequest', (ev) => {
 let timeoutId = null
 function notify(msg) {
   if (timeoutId) clearTimeout(timeoutId)
-  const el = document.querySelector('#notification')
+  const el = document.querySelector("#notification")
   el.innerText = msg
-  el.classList.remove('hideme')
-  timeoutId = setTimeout(() => el.classList.add('hideme'), 3_000)
+  el.classList.remove("hideme")
+  timeoutId = setTimeout(() => el.classList.add("hideme"), 3_000)
 }
