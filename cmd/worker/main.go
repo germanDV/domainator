108300a0f8b81b8a247e9ca8f59fd320ce49b63a
German <germanviescas@gmail.com>
2024-06-01 18:15:44 -0300

    simplify worker (#15)

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
-	"os"
+	"errors"
+	"fmt"
+	"log/slog"
 	"time"
 
+	"github.com/germandv/domainator/internal/cache"
 	"github.com/germandv/domainator/internal/certs"
 	"github.com/germandv/domainator/internal/common"
 	"github.com/germandv/domainator/internal/db"
@@ -13,13 +16,17 @@ import (
 	"github.com/germandv/domainator/internal/users"
 )
 
+const CacheKey = "domainator_worker_running"
+
 type WorkerConfig struct {
 	Env             string `env:"APP_ENV" default:"dev"`
 	LogFormat       string `env:"LOG_FORMAT" default:"text"`
 	LogLevel        string `env:"LOG_LEVEL" default:"info"`
 	PostgresConnStr string `env:"POSTGRES_CONN_STR"`
 	BatchSize       int    `env:"BATCH_SIZE" default:"50"`
-	Concurrency     int    `env:"BATCH_SIZE" default:"20"`
+	RedisHost       string `env:"REDIS_HOST" default:"localhost"`
+	RedisPort       int    `env:"REDIS_PORT" default:"6379"`
+	RedisPassword   string `env:"REDIS_PASSWORD" default:" "`
 }
 
 // This worker is meant to be run as a cron job,
@@ -36,12 +43,53 @@ func main() {
 		panic(err)
 	}
 
+	err = exclusiveRun(config, logger, run)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func exclusiveRun(
+	config *WorkerConfig,
+	logger *slog.Logger,
+	fn func(config *WorkerConfig, logger *slog.Logger) error,
+) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	cacheClient := cache.New(config.RedisHost, config.RedisPort, config.RedisPassword)
+
+	alreadyRunning, err := cacheClient.Get(ctx, CacheKey)
+	if err != nil && !errors.Is(err, cache.ErrNoKey) {
+		return err
+	}
+
+	if alreadyRunning == "true" {
+		logger.Info("Another instance of the worker is already running, exiting")
+		return nil
+	}
+
+	err = cacheClient.Set(ctx, CacheKey, "true", 10*time.Minute)
+	if err != nil {
+		return err
+	}
+
+	err = fn(config, logger)
+
+	e := cacheClient.Set(context.Background(), CacheKey, "false", 0)
+	if e != nil {
+		logger.Warn("failed to set cache key to false", "error", e.Error())
+	}
+
+	return err
+}
+
+func run(config *WorkerConfig, logger *slog.Logger) error {
 	logger.Info("Starting worker")
 
 	db, err := db.InitWithConnStr(config.PostgresConnStr)
 	if err != nil {
-		logger.Error("Failed to connect to database", "error", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to connect to database: %s", err)
 	}
 
 	certsRepo := certs.NewRepo(db)
@@ -61,8 +109,8 @@ func main() {
 		err = certsService.ProcessBatch(
 			context.Background(),
 			config.BatchSize,
-			config.Concurrency,
 			notificationCh,
+			logger,
 		)
 		if err != nil {
 			errCh <- err
@@ -74,32 +122,33 @@ func main() {
 	for {
 		select {
 		case err := <-errCh:
-			logger.Error("Failed to process batch", "error", err)
-			os.Exit(1)
+			return fmt.Errorf("failed to process batch: %s", err)
 		case <-doneCh:
 			logger.Info("Batch processed successfully")
-			os.Exit(0)
+			return nil
 		case n := <-notificationCh:
 			logger.Debug("Sending notification", "domain", n.Domain, "status", n.Status, "hours", n.Hours)
 			userID, err := common.ParseID(n.UserID)
 			if err != nil {
 				logger.Error("Failed to parse user ID", "id", n.UserID, "error", err.Error())
-				return
+				continue
 			}
 
 			user, err := usersService.GetByID(context.Background(), users.GetByIDReq{UserID: userID})
 			if err != nil {
 				logger.Error("Failed to fetch user data", "id", n.UserID, "error", err.Error())
-				return
+				continue
 			}
 
 			if user.WebhookURL.String() == "" {
 				logger.Debug("User has not provided a webhook, skipping notification", "id", n.UserID)
-				return
+				continue
 			}
 
 			err = slacker.Notify(user.WebhookURL.String(), n)
-			logger.Error("Failed to send notification", "id", n.UserID, "webhook_url", user.WebhookURL.String(), "error", err.Error())
+			if err != nil {
+				logger.Error("Failed to send notification", "id", n.UserID, "webhook_url", user.WebhookURL.String(), "error", err.Error())
+			}
 		}
 	}
 }
--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,17 +2,22 @@ package cache
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
+var ErrNoKey = errors.New("key not found")
+
 type Client interface {
 	Ping() error
 	Close() error
 	Increment(key string) (int64, error)
 	Expire(key string, duration time.Duration) error
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value string, ttl time.Duration) error
 }
 
 type RedisClient struct {
@@ -37,6 +42,21 @@ func (rc *RedisClient) Increment(key string) (int64, error) {
 
 func (rc *RedisClient) Expire(key string, duration time.Duration) error {
 	return rc.client.Expire(context.Background(), key, duration).Err()
+}
+
+func (rc *RedisClient) Get(ctx context.Context, key string) (string, error) {
+	val, err := rc.client.Get(ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return "", ErrNoKey
+		}
+		return "", err
+	}
+	return val, nil
+}
+
+func (rc *RedisClient) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
+	return rc.client.Set(ctx, key, value, ttl).Err()
 }
 
 func New(host string, port int, password string) Client {
